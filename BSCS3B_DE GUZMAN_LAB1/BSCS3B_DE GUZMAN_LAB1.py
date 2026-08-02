3126c1d6f406ce9780082fb809a9b40b1809f0fb
valleryuh <valeriedeguzman314@gmail.com>
2026-08-02 23:12:22 +0800

    Add files via upload

---
+++ BSCS3B_DE GUZMAN_LAB1/BSCS3B_DE GUZMAN_LAB1.py
@@ -0,0 +1,8 @@
+#First Python Program CSST-101
+
+print("Name: Valerie De Guzman")
+print("Course: Bachelor of Science in Computer Science")
+print("Section: BSCS-3B")
+print("Favorite AI Technology: Autonomous Biomedical discovery Engine.")
+print("Future AI project: AI Medical Symptom Checker")
+
